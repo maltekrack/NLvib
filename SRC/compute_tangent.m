b1006fbefc8bddb6c001adb0e0fbea05c28eb41b
JohannGross <johann.gross@ila.uni-stuttgart.de>
2024-01-31 10:54:11 +0100

    NLvib-PEACE, initial commit

--- SRC/compute_tangent.m
+++ SRC/compute_tangent.m
@@ -1,5 +1,27 @@
-% ====================================
-% compute new tangent
+%========================================================================
+% DESCRIPTION: 
+% Matlab function for the computation of the local tangent. This function
+% is used in <solve_and_continue.m> and <peace.m>.
+%========================================================================
+% This file is part of NLvib.
+% 
+% If you use NLvib, please refer to the book:
+%   M. Krack, J. Gross: Harmonic Balance for Nonlinear Vibration
+%   Problems. Springer, 2019. https://doi.org/10.1007/978-3-030-14023-6.
+% 
+% COPYRIGHT AND LICENSING: 
+% NLvib Version 1.4 Copyright (C) 2024  Malte Krack  
+%                                       (malte.krack@ila.uni-stuttgart.de) 
+%                                       Johann Gross 
+%                                       (johann.gross@ila.uni-stuttgart.de)
+%                                       University of Stuttgart
+% This program comes with ABSOLUTELY NO WARRANTY. 
+% NLvib is free software, you can redistribute and/or modify it under the
+% GNU General Public License as published by the Free Software Foundation,
+% either version 3 of the License, or (at your option) any later version.
+% For details on license and warranty, see http://www.gnu.org/licenses
+% or gpl-3.0.txt.
+%========================================================================
 function [tangent] = compute_tangent(J,X_0,tangent_0)
 
 [~,kk] = sort(abs(tangent_0./max(abs(X_0),1e-4)),...
--- SRC/extended_residual.m
+++ SRC/extended_residual.m
@@ -1,4 +1,27 @@
-%% extended_residual
+%========================================================================
+% DESCRIPTION: 
+% Matlab function for the computation of the extended residuum. 
+% This function is used in <solve_and_continue.m> and <peace.m>.
+%========================================================================
+% This file is part of NLvib.
+% 
+% If you use NLvib, please refer to the book:
+%   M. Krack, J. Gross: Harmonic Balance for Nonlinear Vibration
+%   Problems. Springer, 2019. https://doi.org/10.1007/978-3-030-14023-6.
+% 
+% COPYRIGHT AND LICENSING: 
+% NLvib Version 1.4 Copyright (C) 2024  Malte Krack  
+%                                       (malte.krack@ila.uni-stuttgart.de) 
+%                                       Johann Gross 
+%                                       (johann.gross@ila.uni-stuttgart.de)
+%                                       University of Stuttgart
+% This program comes with ABSOLUTELY NO WARRANTY. 
+% NLvib is free software, you can redistribute and/or modify it under the
+% GNU General Public License as published by the Free Software Foundation,
+% either version 3 of the License, or (at your option) any later version.
+% For details on license and warranty, see http://www.gnu.org/licenses
+% or gpl-3.0.txt.
+%========================================================================
 function [Rext,dRextdX] = extended_residual(X,Xref,zref,...
     fun_residual,Sopt)
 %% Evaluation of the residual function and its derivative
